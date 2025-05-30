c9b964e538c26366a14793aa72997a71d85d9d56
Hrushikesh0804 <kotturhrushikeshreddy@gmail.com>
2025-05-30 19:13:01 +0530

    Delete src directory

--- src/api/models/History.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const HistorySchema = new mongoose.Schema({
-  email: String,
-  message: String,
-  timestamp: { type: Date, default: Date.now },
-});
-
-export default mongoose.models.History || mongoose.model("History", HistorySchema);
